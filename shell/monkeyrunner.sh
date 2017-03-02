91eb763e418f1b5cf2fa22f3acbb1869398f4b29
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-03-02 16:18:11 +0000

    Attempt to get all devices to be in sync

--- shell/monkeyrunner.sh
+++ shell/monkeyrunner.sh
@@ -9,15 +9,26 @@ DELAY=$2
 # This should come in when this script is called
 EVENT_COUNT=$3
 
-# Install the app on all connected devices
+# Uninstall the app on all connected devices
 for SERIAL in $(adb devices | tail -n +2 | cut -sf 1);
 do
   echo "*** Clearing data for device: " + $SERIAL;
   adb -s $SERIAL shell pm clear $PACKAGE
 
+  echo "*** Uninstall the package if it already exists";
+  adb -s $SERIAL uninstall $PACKAGE
+done
+
+# Install the app on all connected devices
+for SERIAL in $(adb devices | tail -n +2 | cut -sf 1);
+do
   echo "*** Installing application to device: " + $SERIAL;
   adb -s $SERIAL install -r $APK
+done
 
+# Test all the app on all connected devices
+for SERIAL in $(adb devices | tail -n +2 | cut -sf 1);
+do
   echo "*** Running MonkeyRunner on device: " + $SERIAL;
   adb -s $SERIAL shell monkey -p $PACKAGE --throttle $DELAY --monitor-native-crashes --ignore-crashes $EVENT_COUNT &
 done
