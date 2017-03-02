e45e95872185922837c1e3d0c590dfd1398b79dc
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-03-02 11:34:34 +0000

    Switched install scripts to account for new OS

--- setup/android-tools.sh
+++
@@ -1,2 +0,0 @@
-# https://developer.android.com/studio/index.html
-curl -O https://dl.google.com/android/repository/tools_r25.2.3-linux.zip && unzip tools_r25.2.3-linux.zip
--- setup/java.sh
+++
@@ -1,2 +0,0 @@
-# https://wiki.debian.org/Java
-apt-get install default-jdk
